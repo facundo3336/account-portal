30227401e66565fe85aa3c969560a8fb7bdea7aa
facundo3336 <fcv3336@gmail.com>
2022-08-31 14:25:02 -0300

    Login page ready

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,6 +3,9 @@ import { Card } from "../../components/Card/Card";
 import { NextPage } from "next";
 import { Input } from "../../components/Input/Input";
 import styles from "./Login.module.scss";
+import { Checkbox } from "../../components/Checkbox/CheckBox";
+import { Button, ButtonColor } from "../../components/Button/Button";
+import Link from "next/link";
 
 const Login: NextPage = () => {
   const [data, setData] = useState({
@@ -25,27 +28,40 @@ const Login: NextPage = () => {
   };
 
   return (
-    <div className={styles.loginPage}>
-      <Card size="md">
-        <div>
-          <h1>Ingresa a tu cuenta</h1>
-          <div className={styles.inputsContainer}>
-            <Input
-              type="email"
-              label="Email"
-              value={data.email}
-              onChange={onChangeEmail}
-            />
-            <Input
-              type="text"
-              label="Contraseña"
-              value={data.name}
-              onChange={onChangeName}
-              link={{ label: "Olvidaste tu contraseña?", url: "" }}
-            />
+    <div className={styles.loginPage + " sideSpacing"}>
+      <div className={styles.loginPageContent}>
+        <Card size="md">
+          <div>
+            <h1>Ingresa a tu cuenta</h1>
+            <div className={styles.inputsContainer}>
+              <Input
+                type="email"
+                label="Email"
+                value={data.email}
+                onChange={onChangeEmail}
+              />
+              <Input
+                type="text"
+                label="Contraseña"
+                value={data.name}
+                onChange={onChangeName}
+                link={{ label: "Olvidaste tu contraseña?", url: "" }}
+              />
+              <div className={styles.checkBoxLoginContainer}>
+                <Checkbox label="Recordarme" />
+              </div>
+              <Button color={ButtonColor.Primary} onClick={() => {}}>
+                Continuar
+              </Button>
+            </div>
           </div>
+        </Card>
+        <div className={styles.accountAskContainer}>
+          <span className={styles.accountAsk}>
+            No tienes una cuenta? <Link href="/">Crea una</Link>
+          </span>
         </div>
-      </Card>
+      </div>
     </div>
   );
 };
