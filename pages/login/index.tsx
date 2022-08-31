dbaca381a70d852c806e04d1ccf9407cce0006ea
facundo3336 <fcv3336@gmail.com>
2022-08-31 16:41:52 -0300

    Get token in login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,17 +6,18 @@ import styles from "./Login.module.scss";
 import { Checkbox } from "../../components/Checkbox/CheckBox";
 import { Button, ButtonColor } from "../../components/Button/Button";
 import Link from "next/link";
+import { User } from "../../types";
 
 const Login: NextPage = () => {
-  const [data, setData] = useState({
-    name: "",
+  const [data, setData] = useState<User>({
     email: "",
+    password: "",
   });
 
-  const onChangeName = (value: string) => {
+  const onChangePassword = (value: string) => {
     setData({
       ...data,
-      name: value,
+      password: value,
     });
   };
 
@@ -25,6 +26,10 @@ const Login: NextPage = () => {
       ...data,
       email: value,
     });
+  };
+
+  const onClickLogUser = () => {
+    getToken(data);
   };
 
   return (
@@ -43,14 +48,14 @@ const Login: NextPage = () => {
               <Input
                 type="text"
                 label="Contraseña"
-                value={data.name}
-                onChange={onChangeName}
+                value={data.password}
+                onChange={onChangePassword}
                 link={{ label: "Olvidaste tu contraseña?", url: "" }}
               />
               <div className={styles.checkBoxLoginContainer}>
                 <Checkbox label="Recordarme" />
               </div>
-              <Button color={ButtonColor.Primary} onClick={() => {}}>
+              <Button color={ButtonColor.Primary} onClick={onClickLogUser}>
                 Continuar
               </Button>
             </div>
@@ -64,6 +69,26 @@ const Login: NextPage = () => {
       </div>
     </div>
   );
+};
+
+export const getToken = async (data: User) => {
+  const tokenResponse = await fetch(`http://localhost:3000/auth/login`, {
+    method: "POST",
+    body: JSON.stringify({
+      username: data.email,
+      password: data.password,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+
+  const tokenData = await tokenResponse.json();
+
+  if (tokenResponse.status === 201) {
+    localStorage.setItem("access_token", tokenData.access_token);
+  }
 };
 
 export default Login;
