d67b04f2901b22f960f7b446b64e7a7a38d4744a
facundo3336 <fcv3336@gmail.com>
2022-09-08 16:23:29 -0300

    Create Loggedin component

---
+++ components/LoggedIn/LoggedIn.tsx
@@ -0,0 +1,34 @@
+import Router from "next/router";
+import jwt_decode from "jwt-decode";
+import { useEffect, useState } from "react";
+
+interface Props {
+  children: JSX.Element;
+}
+
+export const LoggedIn = ({ children }: Props) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token") as string;
+    const decoded = jwt_decode<{ exp: number }>(token);
+
+    const tokenExpiresAt = new Date(decoded.exp * 1000);
+    console.log(tokenExpiresAt);
+    const now = new Date();
+    console.log(now);
+
+    if (tokenExpiresAt > now) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+      Router.push("/login");
+    }
+  }, [loggedIn]);
+
+  if (loggedIn) {
+    return children;
+  }
+
+  return null;
+};
